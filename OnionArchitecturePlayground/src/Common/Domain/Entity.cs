7a2eada5b894100775f818918ea31110700d6024
Katsukiniwa <katsukiniwa0217@gmail.com>
2021-09-04 18:21:15 +0900

    create src

---
+++ OnionArchitecturePlayground/src/Common/Domain/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OnionArchitecturePlayground.src.Common.Domain
+{
+    public abstract class Entity
+    {
+        string _Id;
+        public virtual string Id
+        {
+            get
+            {
+                return _Id;
+            }
+            protected set
+            {
+                _Id = value;
+            }
+        }
+
+        private List<INotification> _domainEvents;
+        public IReadOnlyCollection<INotification> DomainEvents => _domainEvents?.AsReadOnly();
+    }
+}
